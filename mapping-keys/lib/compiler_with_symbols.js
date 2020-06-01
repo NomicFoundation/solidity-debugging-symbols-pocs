f19ac82eeba78fc9ed01302889f4871b03601e08
Juan Franco <jcaracciolo@itba.edu.ar>
2020-06-01 16:40:20 -0300

    Add the solc compiler with symbols

---
+++ mapping-keys/lib/compiler_with_symbols.js
@@ -0,0 +1,82 @@
+const { exec } = require("child_process");
+const fs = require('fs').promises
+
+async function sh(cmd) {
+    return new Promise(function (resolve, reject) {
+        exec(cmd, (err, stdout, stderr) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve({ stdout, stderr });
+            }
+        });
+    });
+}
+
+async function compile(path) {
+    return sh("lib/compiler/solcDebugSym --bin " + path).then( ({ stdout }) => {
+            const bytecode = stdout.split("\n")[3];
+
+            return Promise.all([
+                fs.readFile("mappings.json", 'utf8'),
+                fs.readFile("mappingsOffset.tsv", 'utf8'),
+                fs.readFile("variables.json", 'utf8'),
+                fs.readFile("variablesOffset.tsv", 'utf8')
+            ]).then( async ([mappingsJson, mappingsOffsetTsv, variablesJson, variablesOffsetTsv]) => {
+
+                let regex = /,(?!\s*?[{\["'\w])/g;            // stack overflow magic
+                mappingsJson = mappingsJson.replace(regex, '');   // remove all trailing commas
+                variablesJson = variablesJson.replace(regex, ''); // remove all trailing commas
+
+                const mappingsOffsets = mappingsOffsetTsv.split("\n")
+                    .map(n => n.split("\t"))
+                    .reduce((o, v) => {
+                        o[v[0]] = {};
+                        o[v[0]].bytecodeOffset = v[1];
+                        o[v[0]].deployedBytecodeOffset = v[2] === 'null' ? undefined : v[2];
+                        return o;
+                    }, {});
+
+                const variablesOffsets = variablesOffsetTsv.split("\n")
+                    .map(n => n.split("\t"))
+                    .reduce((o, v) => {
+                        o[v[0]] = {};
+                        o[v[0]].bytecodeOffset = v[1];
+                        o[v[0]].deployedBytecodeOffset = v[2] === 'null' ? undefined : v[2];
+                        return o;
+                    }, {});
+
+                const mappings = JSON.parse(mappingsJson).map(v => {
+                    v.bytecodeOffset = mappingsOffsets[v.id].bytecodeOffset;
+                    v.deployedBytecodeOffset = mappingsOffsets[v.id].deployedBytecodeOffset;
+                    return v;
+                });
+
+                const variables = JSON.parse(variablesJson).map(v => {
+                    v.bytecodeOffset = variablesOffsets[v.id].bytecodeOffset;
+                    v.deployedBytecodeOffset = variablesOffsets[v.id].deployedBytecodeOffset;
+                    return v;
+                });
+
+                await Promise.all([
+                    fs.unlink("mappings.json"),
+                    fs.unlink("mappingsOffset.tsv"),
+                    fs.unlink("variables.json"),
+                    fs.unlink("variablesOffset.tsv")
+                ]);
+
+                return {
+                    bytecode,
+                    variables,
+                    mappings
+                };
+            })
+
+        }).catch( e =>
+            console.error(`Failed to compile ${path}\n${e}`)
+        )
+}
+
+module.exports = {
+    compile
+};
