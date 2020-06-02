50e13ccef55b7a48a6885831ccb3765afcdc0915
Sebastián Claudio Nale <sebastian@nomiclabs.io>
2020-06-02 14:58:33 -0300

    Remove obsolete build script.

--- mapping-keys/build_contracts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SOLC="solc"
-SOLC_FLAGS="--combined-json abi,asm,ast,bin,bin-runtime,compact-format,devdoc,hashes,interface,metadata,opcodes,srcmap,srcmap-runtime,storage-layout,userdoc"
-
-$SOLC $SOLC_FLAGS contracts/MapLayoutExplorer.sol > artifacts/MapLayoutExplorer.json
-$SOLC $SOLC_FLAGS contracts/ASTExplorer.sol > artifacts/ASTExplorer.json
