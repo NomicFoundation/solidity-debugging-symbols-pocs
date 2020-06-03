89af53380df01c64f7752983eb4d837ce7d295d1
Sebastián Claudio Nale <sebastian@nomiclabs.io>
2020-06-03 12:37:53 -0300

    Add ganache seed to fix generated addresses.

--- mapping-keys/buidler.config.js
+++ mapping-keys/buidler.config.js
@@ -7,9 +7,15 @@ usePlugin("@nomiclabs/buidler-web3");
 // defaultNetwork, networks, solc, and paths.
 // Go to https://buidler.dev/config/ to learn more
 module.exports = {
-  // This is a sample solc configuration that specifies which version of solc to use
-  solc: {
-    version: "0.6.7",
-  },
-  defaultNetwork: "ganache"
+    // This is a sample solc configuration that specifies which version of solc to use
+    solc: {
+        version: "0.6.7",
+    },
+    defaultNetwork: "ganache",
+    networks: {
+        ganache: {
+            url: "http://localhost:8545",
+            seed: "one symbol format to rule them all"
+        }
+    }
 };
